cab5d5fdecfd118b89e5dcb2b06a4624b8422554
Ytrer15530 <ilya15530@gmail.com>
2023-09-08 22:37:51 +0300

    Domashka Lesson 4 Task 4

--- Domashka_4_Lesson.py
+++ Domashka_4_Lesson.py
@@ -29,4 +29,15 @@ what_to = input("On what word you want to change?: ").lower()
 print(f"Your string is -> {string_input.replace(what_word, what_to).title()}")
 
 
+# Task 4
 
+user_input_string = input("Enter string: ")
+print(f"Третий символ -> {user_input_string[2]}")
+print(f"Предпоследний символ строки -> {user_input_string[-2]}")
+print(f"Первые 5 символов -> {user_input_string[:5]}")
+print(f"Строка без 2-х последних символов -> {user_input_string[:-2]}")
+print(f"Парные индексы -> {user_input_string[::2]}")
+print(f"Непарные индексы -> {user_input_string[1::2]}")
+print(f"Строка наоборот -> {user_input_string[::-1]}")
+print(f"Строка наоборот через 1 -> {user_input_string[::-2]}")
+print(f"Длина строки -> {len(user_input_string)}")
