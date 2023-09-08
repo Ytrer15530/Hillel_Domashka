8796ae74f013670f9cb752a13a0cd391f3177929
Ytrer15530 <ilya15530@gmail.com>
2023-09-08 21:55:21 +0300

    Domashka 4 task 2

--- Domashka_4_Lesson.py
+++ Domashka_4_Lesson.py
@@ -1,12 +1,24 @@
-user_string = input("Enter string: ")
-letters = 0
-numbers = 0
+# user_string = input("Enter string: ")
+# letters = 0
+# numbers = 0
+#
+# for i in user_string:
+#     if i.isalpha():
+#         letters += 1
+#     elif i.isnumeric():
+#         numbers += 1
+# print(f"Letters in string -> {letters}, numbers in string -> {numbers}")
 
-for i in user_string:
-    if i.isalpha():
-        letters += 1
-    elif i.isnumeric():
-        numbers += 1
-print(f"Letters in string -> {letters}, numbers in string -> {numbers}")
+
+# TASK 2
+
+string_input = input("Enter string: ")
+symbol_search = input("What symbol to search?: ")
+symbol_count = 0
+
+for i in string_input:
+    if i == symbol_search:
+        symbol_count += 1
+print(f"In string {symbol_count} {symbol_search}'s")
 
 
