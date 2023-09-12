72eccddc79cb89b499752f17327cfa03452640fc
Ytrer15530 <ilya15530@gmail.com>
2023-09-12 23:41:11 +0300

    Lesson 5 сумма парных / непарных

--- Domashka_5.py
+++ Domashka_5.py
@@ -4,6 +4,7 @@ random_list = []
 sum_neg = 0
 negative_list = []
 even_sum = 0
+odd_sum = 0
 
 for i in range(10):
     random_number = random.randint(-10, 10)
@@ -19,3 +20,14 @@ for i in negative_list:
     sum_neg += i
 print(f"Сумма негативных чисел равна -> {sum_neg}")
 
+for i in random_list:
+    if i % 2 == 0:
+        even_sum += i
+        # print(i, end=" ")
+print(f"Сумма парных чисел равна -> {even_sum}")
+
+for i in random_list:
+    if i % 2 != 0:
+        odd_sum += i
+        # print(i, end=" ")
+print(f"Сумма непарных чисел равна -> {odd_sum}")
