40ffabccf3bc4351fd603aa85f5652ad68f0776d
Ytrer15530 <ilya15530@gmail.com>
2023-09-19 21:53:41 +0300

    Domashka 7 поиск минимума

--- Domashka_7.py
+++ Domashka_7.py
@@ -1,4 +1,6 @@
 import random
+MAX_RANDOM_NUMBER = 10
+MIN_RANDOM_NUMBER = 1
 
 
 def mult_list(numbers: list):
@@ -8,7 +10,14 @@ def mult_list(numbers: list):
     return numbers_mult
 
 
-random_numbers_list = [random.randint(1, 10) for i in range(10)]
+def find_minimum(numbers: list):
+    min_number = min(numbers)
+    return min_number
+
+
+random_numbers_list = [random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) for i in range(10)]
+
 print(random_numbers_list)
 print(f"Произведение списка -> {mult_list(random_numbers_list)}")
 
+print(f"Минимум в списке -> {find_minimum(random_numbers_list)}")
