39e178d57f9a7e14f0bdc29b4b2377508d4153b7
Ytrer15530 <ilya15530@gmail.com>
2023-09-20 14:46:29 +0300

    Domashka 7 простые числа

--- Domashka_7.py
+++ Domashka_7.py
@@ -15,9 +15,28 @@ def find_minimum(numbers: list):
     return min_number
 
 
-random_numbers_list = [random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) for i in range(10)]
+prime_nums = []
 
+
+def find_prime_numbers(numbers: list):
+    for num in numbers:
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+                else:
+                    prime_nums.append(num)
+                    break
+    return prime_nums
+
+
+def len_prime_list(prime_list: list):
+    return len(prime_list)
+
+
+random_numbers_list = [random.randint(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER) for i in range(10)]
 print(random_numbers_list)
 print(f"Произведение списка -> {mult_list(random_numbers_list)}")
-
 print(f"Минимум в списке -> {find_minimum(random_numbers_list)}")
+# print(find_prime_numbers(random_numbers_list))
+print(f"Количество простых чисел в списке -> {len_prime_list(find_prime_numbers(random_numbers_list))}")
