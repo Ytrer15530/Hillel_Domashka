0958e8238502223d61ddd518d482fbac89637446
Ytrer15530 <ilya15530@gmail.com>
2023-09-22 23:38:03 +0300

    Домашка 8 диапазон сумма

--- Domashka_8.py
+++ Domashka_8.py
@@ -28,3 +28,24 @@ star_line(star_int)
 # n2 > 0 -> **
 # n1 > 0 -> ***
 # n 0 = 0 end
+
+
+int_a = int(input("\nEnter number for a: "))
+int_b = int(input("Enter number for b: "))
+
+
+def sum_between(a, b):
+    if a > b:
+        return 0
+    return a + sum_between(a+1, b)
+
+
+print(f"Сумма чисел между {int_a} и {int_b} -> {sum_between(int_a, int_b)}")
+
+# a=1 b =3
+# 1 < 3 -> a = 1
+# 2 < 3 -> a = 2
+# 3 !> 3 -> a = 3
+# 0 + 1 -> 1 
+# 1 + 2 -> 3
+# 3 + 3 -> 6
