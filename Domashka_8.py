950172c2d6f205d3cf6c11e4267dd3a4563b9ae2
Ytrer15530 <ilya15530@gmail.com>
2023-09-22 22:58:32 +0300

    Домашка рекурсия *

--- Domashka_8.py
+++ Domashka_8.py
@@ -9,3 +9,22 @@ print(mult_rec(3, 3))
 # 3 * mult_rec(3, 2) -> 9 * 3 -> 27
 # 3 * mult_rec(3, 1) -> 3 * 3 -> 9
 # 3
+
+star_int = int(input("Enter number of stars: "))
+
+
+def star_line(n):
+    if n <= 0:
+        return
+    else:
+        print("*", end="")
+        star_line(n-1)
+
+
+star_line(star_int)
+
+# n = 3
+# n3 > 0 -> *
+# n2 > 0 -> **
+# n1 > 0 -> ***
+# n 0 = 0 end
