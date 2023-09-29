bd9e50a4b3cfa0cc75877f47fb2800687a758138
Ytrer15530 <ilya15530@gmail.com>
2023-09-29 22:57:38 +0300

    Домашка 10 моб.телефон

--- Domashka_10.py
+++ Domashka_10.py
@@ -7,3 +7,9 @@ elif re.match(r'\d', tel_numb) and len(tel_numb) == 10:
     print(f"{tel_numb} is OK!")
 elif len(tel_numb) != 10:
     print("Number must consist of 10 digits!")
+
+ua_tel = input("Enter your number: ")
+if re.findall("[a-zA-Z!@#$%^&*()_=]", ua_tel) or 12 > len(ua_tel) > 13:
+    print("Incorrect number!")
+elif re.match(r"^[+]?\d", ua_tel) and 12 <= len(ua_tel) <= 13:
+    print(f"{ua_tel} OK")
